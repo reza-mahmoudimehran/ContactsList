d1a4583aa11b149d9925fb6f806f02995b8b1319
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2023-04-30 01:41:17 +0430

    Setup Hilt

---
+++ app/src/main/java/ir/reza_mahmoudi/contactslist/ContactsListApp.kt
@@ -0,0 +1,7 @@
+package ir.reza_mahmoudi.contactslist
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ContactsListApp : Application()
--- app/src/main/java/ir/reza_mahmoudi/contactslist/core/presentation/MainActivity.kt
+++ app/src/main/java/ir/reza_mahmoudi/contactslist/core/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package ir.reza_mahmoudi.contactslist
+package ir.reza_mahmoudi.contactslist.core.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import ir.reza_mahmoudi.contactslist.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
