3c89aa28fae5fd628c4477ae5f21bb28fe5148f0
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2023-04-30 04:19:41 +0430

    Add GetContactsListUseCase

---
+++ app/src/main/java/ir/reza_mahmoudi/contactslist/feature_contacts/domain/get_contacts_list/GetContactsListUseCase.kt
@@ -0,0 +1,19 @@
+package ir.reza_mahmoudi.contactslist.feature_contacts.domain.get_contacts_list
+
+import ir.reza_mahmoudi.contactslist.core.di.qualifiers.IoDispatcher
+import ir.reza_mahmoudi.contactslist.core.domain.common.usecase.FlowUseCase
+import ir.reza_mahmoudi.contactslist.feature_contacts.domain.ContactsRepository
+import ir.reza_mahmoudi.contactslist.feature_contacts.domain.common.entity.ContactEntity
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetContactsListUseCase @Inject constructor(
+    private val contactsRepository: ContactsRepository,
+    @IoDispatcher ioDispatcher: CoroutineDispatcher
+) : FlowUseCase<Unit, List<ContactEntity>>(
+    ioDispatcher
+) {
+    override fun execute(parameters: Unit): Flow<List<ContactEntity>> =
+        contactsRepository.getContactsList()
+}
