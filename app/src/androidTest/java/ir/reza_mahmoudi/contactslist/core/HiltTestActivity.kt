6a2f84d923478285e60004f1c5d387c053cfc064
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2023-05-03 00:31:50 +0430

    Setup Hilt Test Runner

---
+++ app/src/androidTest/java/ir/reza_mahmoudi/contactslist/core/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package ir.reza_mahmoudi.contactslist.core
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
